107efc59479ecbf0b899836a2e675ca7bc5e6d2a
Connor Ray & Dom Brassey <pair+cray9503.domlet@devbootcamp.com>
2016-08-26 21:29:21 -0700

    testing api

--- api_test.rb
+++ api_test.rb
@@ -9,6 +9,11 @@ class SpotifyArtist
 
   def initialize(artist)
     @artist_search = artist
+    @json = nil
+    @id = nil
+    @artist_data = []
+    @related_artists = []
+    @ra_json = nil
   end
 
   def search_prep(artist)
@@ -25,26 +30,52 @@ class SpotifyArtist
 
   def search
     name = search_prep(@artist_search)
-    self.class.get("/search?q=#{name}&type=artist", header: {"Accept" => "application/json"})
+    @json = self.class.get("/search?q=#{name}&type=artist", header: {"Accept" => "application/json"})
   end
-end
 
-def parse_data(json_object)
-  data = []
-  object_hash = json_object.first[1]['items'][0]
-  data << object_hash["name"]
-  data << object_hash["popularity"]
-  data << object_hash["id"]
-  data << object_hash["genres"]
+  def parse_data
+    object_hash = @json.first[1]['items'][0]
+    @artist_data << object_hash["name"]
+    @artist_data << object_hash["popularity"]
+    @artist_data << object_hash["id"]
+    @artist_data << object_hash["genres"]
+  end
+
+  def parse_related_artists
+
+  end
+
+  def related_artists
+    self.search
+    self.parse_data
+    id = @artist_data[2]
+    id
+    @ra_json = self.class.get("/artists/#{id}/related-artists", header: {"Accept" => "application/json"})
+  end
 end
 
 search = "Rolling Stones"
 
-rstone = SpotifyArtist.new(search)
-search = rstone.search
-artist_data = parse_data(search)
-p artist_data
-# a = search.first[1]['items'][0]
-# p a.class
+rolling = SpotifyArtist.new(search)
+
+a = rolling.related_artists
+p a.first
+# def parse_data(json_object)
+#   data = []
+#   object_hash = json_object.first[1]['items'][0]
+#   data << object_hash["name"]
+#   data << object_hash["popularity"]
+#   data << object_hash["id"]
+#   data << object_hash["genres"]
+# end
+
+# search = "Rolling Stones"
+
+# rstone = SpotifyArtist.new(search)
+# search = rstone.search
+# artist_data = parse_data(search)
+# p artist_data
+# # a = search.first[1]['items'][0]
+# # p a.class
 
 # ["external_urls", "followers", "genres", "href", "id", "images", "name", "popularity", "type", "uri"]
