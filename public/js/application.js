a58f78ffc86826f8d961ecc9e61d863bcee12a71
Connor Ray <connor.ray.cb@gmail.com>
2016-09-04 19:04:51 -0700

    killing it

--- public/js/application.js
+++ public/js/application.js
@@ -79,13 +79,10 @@ $(document).ready(function () {
 });
 
 function openNav() {
-  console.log("yayyy")
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main_content").style.marginRight = "250px";
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+/* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main_content").style.marginRight = "0";
 }
